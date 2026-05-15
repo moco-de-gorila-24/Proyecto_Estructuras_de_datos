a3ff6dfadb858e61b978837be8708945db0e6797
Luis Alonso <luisalonsoortiz343@gmail.com>
2026-05-15 16:31:17 -0700

    Commit inicial

---
+++ src/main/java/com/mycompany/proyecto_final_estructura/Proyecto_Final_Estructura.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_final_estructura;
+
+/**
+ *
+ * @author Central
+ */
+public class Proyecto_Final_Estructura {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
