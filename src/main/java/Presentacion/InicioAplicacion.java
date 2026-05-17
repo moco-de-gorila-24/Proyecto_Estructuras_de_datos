ec2ab48bb33ac25d63ccfaa5c2e7554663768228
Luis Alonso <luisalonsoortiz343@gmail.com>
2026-05-16 23:06:51 -0700

    Arregle el error merge

--- src/main/java/Presentacion/InicioAplicacion.java
+++ src/main/java/Presentacion/InicioAplicacion.java
@@ -8,6 +8,10 @@ package Presentacion;
 public class InicioAplicacion {
 
     public static void main(String[] args) {
+<<<<<<< Updated upstream:src/main/java/Presentacion/InicioAplicacion.java
         //aqui ponemos para iniciar el flujo de las pantallas ya que las tengamos
+=======
+
+>>>>>>> Stashed changes:src/main/java/com/mycompany/proyecto_final_estructura/Proyecto_Final_Estructura.java
     }
 }
