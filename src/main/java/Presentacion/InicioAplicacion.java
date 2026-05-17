415a17fe7119321abb0c3871b506bdd4085eabd1
Luis Alonso <luisalonsoortiz343@gmail.com>
2026-05-16 23:08:47 -0700

    Ahora si el arregle el error merge
    se me olvido hacer el add

--- src/main/java/Presentacion/InicioAplicacion.java
+++ src/main/java/Presentacion/InicioAplicacion.java
@@ -8,10 +8,7 @@ package Presentacion;
 public class InicioAplicacion {
 
     public static void main(String[] args) {
-<<<<<<< Updated upstream:src/main/java/Presentacion/InicioAplicacion.java
         //aqui ponemos para iniciar el flujo de las pantallas ya que las tengamos
-=======
 
->>>>>>> Stashed changes:src/main/java/com/mycompany/proyecto_final_estructura/Proyecto_Final_Estructura.java
     }
 }
